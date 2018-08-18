3d3272c6cc92dce01365cb2265e0c9a96ed7b00f
mrmanjunathams <mrmanjunathams@gmail.com>
2018-08-18 19:58:46 +0530

    third commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var app =angular.module("myApp",["ngRoute"]);
+
+app.config(function($routeProvider){
+    $routeProvider.when("/",{
+      templateUrl:"templets/home.html"
+    }).when("/top",{
+        templateUrl:"templets/top.html"
+    }).when("/worldmap",{
+        templateUrl:"templets/worldmap.html"
+    }).when("/findplace",{
+      templateUrl:"templets/findplace.html"
+    }).when("/aboutus",{
+      templateUrl:"templets/aboutus.html"
+    }).when("/contactus",{
+      templateUrl:"templets/contactus.html"
+    }).when("/livechat",{
+      templateUrl:"templets/livechat.html"
+    });
+
+});
